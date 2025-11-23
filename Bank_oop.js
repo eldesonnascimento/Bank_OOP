2bbebf697c6a1387cad0a413473a5bda6dbf2549
eldesonnascimento <122459383+eldesonnascimento@users.noreply.github.com>
2025-11-23 19:03:28 -0300

    Create Bank_oop.js
    A small application of a bank with its business rules.

---
+++ Bank_oop.js
@@ -0,0 +1,47 @@
+class Conta {
+  constructor(){
+    this.saldo = 0;
+  }
+  depositar(deposito){
+    if(deposito > 0){
+    this.saldo += deposito;
+    console.log(`deposito concluído,
+você depositou: ${deposito} Reais\n`)
+    }
+    else{
+      console.log(`
+*********************"******
+depósito negado
+****************************
+você tentou depositar uma quantia menor que 0 Reais.
+****************************\n`)
+    }
+  }
+  sacar(saque){
+    if(this.saldo >= saque && saque > 0){
+    this.saldo -= saque;
+    
+  console.log(`você sacou: ${saque} Reais,
+seu saldo ficou: ${this.saldo} Reais.\n`)
+  
+    }else{
+      console.log(`**********************
+saque negado;
+**********************
+seu saldo é: ${this.saldo} Reais,
+saques permitidos de até o máximo de: ${this.saldo} Reais.
+você tentou sacar: ${saque} Reais.
+**********************\n`)
+    }
+  }
+  valor(){
+    console.log(`seu saldo atualizado é: ${this.saldo} Reais.\n`)
+  }
+}
+
+const conta = new Conta();
+
+conta.depositar(60);
+
+conta.sacar(80);
+
